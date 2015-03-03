2e327bf700508466479ecd36961afb0f25a0e6fa
Kyle Boss <kyle@kylethewizkid.com>
2015-03-02 20:40:38 -0800

    VisibleObj -> MaterialProps

--- rayTracer.cpp
+++ rayTracer.cpp
@@ -11,7 +11,7 @@
 #include "Coord.h"
 #include "Camera.h"
 #include "Light.h"
-#include "VisibleObject.h"
+#include "MaterialProps.h"
 #include "Triangle.h"
 #include "Ray.h"
 // #include "objParse.cpp"
@@ -94,7 +94,7 @@ int main (int argc, char *argv[]) {
   Canvas canvas = Canvas(400.0, 40.0);
   Camera cam = Camera(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
   Light light = Light (1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
-  VisibleObject visObj = VisibleObject(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+  MaterialProps visObj = MaterialProps(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
   Triangle tri = Triangle(1, 2, 3, 4, 5, 6, 7, 8, 9);
   cout << light.color.r;
   cout << "rendering..." << endl;
