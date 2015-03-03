0e70b114864027690abddc68ea88ed3195fc9d35
Declan Jones <declan.jones@berkeley.edu>
2015-03-02 21:15:07 -0800

    Sampler

--- Canvas.h
+++ Canvas.h
@@ -1,9 +1,14 @@
+#include "Sample.h"
+
 class Canvas {
   public:
-     Canvas(double width, double height) : 
-     width(width),
-     height(height)
-     {}
+    Sample currSample;
     double width;
     double height;
+    Canvas(double width, double height) :
+    width(width),
+    height(height),
+    currSample(Sample(0, 0))
+    {}
+    bool getSample(Sample* sample);
 };
---
+++ Sample.h
@@ -0,0 +1,6 @@
+class Sample {
+  public:
+    float x;
+    float y;
+    Sample(float x, float y) : x(x), y(y) {}
+};
