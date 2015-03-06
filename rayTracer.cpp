0da1bbe75ea2c8ccf70d254e4d50709221638188
DeclanJones <declan.jones@berkeley.edu>
2015-03-05 19:50:24 -0800

    Update rayTracer.cpp

--- rayTracer.cpp
+++ rayTracer.cpp
@@ -33,8 +33,8 @@ void render() {
 
 	//SET UP CAMERA
 	Coord loc = Coord(1, 1, 1);
-  Coord viewDir = Coord(2, 2, 2);
-  Camera camera = Camera(loc, 1, viewDir);
+  	Coord viewDir = Coord(2, 2, 2);
+	Camera camera = Camera(loc, 1, viewDir);
 
 	//RENDER LOOP
 	while(canvas.getSample(&canvas.currSample)) {
